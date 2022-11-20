aa1ee910ce6d774e4566ae05d657fd507063bc89
Long Viet Ta <long.tv2222@gmail.com>
2022-11-19 20:16:23 -0700

    Fix HeaderFooterLayout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,31 +3,35 @@ import { useAuth } from "../../Context/AuthProvider";
 import LoginModal from "../../components/LoginModal";
 import SignupModal from "../../components/SignupModal";
 import Button from "@mui/material/Button";
+import { HeaderFooterLayout } from "../../layouts/HeaderFooterLayout/HeaderFooterLayout";
 
 export const Home: React.FC = () => {
     const { user } = useAuth();
     const [isLoginModalOpen, setLoginModalOpen] = React.useState(false);
     const [isSignupModalOpen, setSignupModalOpen] = React.useState(false);
-    return user ? (
-        <h1>Welcome to MatchEd, {user.firstName}</h1>
-    ) : (
-        <>
-            <div>
-                Welcome to MatchEd. Please sign up or login to access all the
-                features
-            </div>
-            <Button variant="contained" onClick={() => setLoginModalOpen(true)}>
-                Login
-            </Button>
-            <Button variant="outlined" onClick={() => setSignupModalOpen(true)}>Sign up</Button>
-            <LoginModal
-                open={isLoginModalOpen}
-                onClose={() => setLoginModalOpen(false)}
-            />
-            <SignupModal
-                open={isSignupModalOpen}
-                onClose={() => setSignupModalOpen(false)}
-            />
-        </>
-    );
+
+    if (user)
+        return (
+            <HeaderFooterLayout>
+                <h1>Welcome to MatchEd, {user.firstName}</h1>
+                <>
+                    <div>
+                        Welcome to MatchEd. Please sign up or login to access all the
+                        features
+                    </div>
+                    <Button variant="contained" onClick={() => setLoginModalOpen(true)}>
+                        Login
+                    </Button>
+                    <Button variant="outlined" onClick={() => setSignupModalOpen(true)}>Sign up</Button>
+                    <LoginModal
+                        open={isLoginModalOpen}
+                        onClose={() => setLoginModalOpen(false)}
+                    />
+                    <SignupModal
+                        open={isSignupModalOpen}
+                        onClose={() => setSignupModalOpen(false)}
+                    />
+                </>
+            </HeaderFooterLayout>
+        )
 };
