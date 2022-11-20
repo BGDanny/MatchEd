dd3d4f8e183c70d5fbadf34cde2f8dfcf75c2ce5
Long Viet Ta <long.tv2222@gmail.com>
2022-11-19 21:04:54 -0700

    Add basic nav bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,30 @@
-
+import { Box, Button } from "@mui/material"
+import logo from './Logo.png';
 
 export const NavBar: React.FC = () => {
-    return (<h1>This is a navbar</h1>)
+    return (<Box sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        mx: {
+            xs: "20px",
+            md: "50px"
+        }
+    }}>
+        <Box
+            component="img"
+            sx={{
+                height: 50,
+            }}
+            alt="Logo"
+            src={logo}
+        />
+        <Box sx={{
+            display: "flex"
+        }}>
+            <Button variant="outlined">Log In</Button>
+            <Button variant="outlined">Sign up</Button>
+        </Box>
+    </Box>)
 }
